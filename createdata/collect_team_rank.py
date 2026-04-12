07beaf66e1607ed443a778c4dc25cd00dd83ada1
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-04-12 21:42:25 +0100

    adding rank for world cup team

--- createdata/collect_team_rank.py
+++ createdata/collect_team_rank.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 import os 
 import csv
+import pandas as pd
+from datetime import datetime
 
 dates = [
     "18/07/2024",
@@ -20,6 +22,8 @@ dates = [
     "18/03/2026",
     "01/04/2026"
 ]
+for i in range(len(dates)):
+    dates[i]=datetime.strptime(dates[i], "%d/%m/%Y").strftime("%Y-%m-%d")
 
 world_cup_2026_teams = [
     # Hosts
@@ -34,7 +38,7 @@ world_cup_2026_teams = [
     "Morocco", "Senegal", "South Africa", "Tunisia",
 
     # CONCACAF (North & Central America, Caribbean)
-    "Curaçao", "Haiti", "Panama",
+    "Curacao", "Haiti", "Panama",
 
     # CONMEBOL (South America)
     "Argentina", "Brazil", "Colombia", "Ecuador", "Paraguay", "Uruguay",
@@ -43,30 +47,49 @@ world_cup_2026_teams = [
     "New Zealand",
 
     # UEFA (Europe)
-    "Austria", "Belgium", "Bosnia and Herzegovina", "Croatia", "Czech Republic",
+    "Austria", "Belgium", "Bosnia and Herzegovina", "Croatia", "Czechia",
     "England", "France", "Germany", "Netherlands", "Norway",
     "Portugal", "Scotland", "Spain", "Sweden", "Switzerland", "Turkey",
 
     # Play-Off Tournament winners
-    "Bolivia", "DR Congo", "Iraq", "Jamaica", "New Caledonia", "Suriname"
+    "Bolivia", "Congo DR", "Iraq"
 ]
 
 class team:
-    def __init__(self,name):
+    def __init__(self,name,rank,point,conf):
         self.name=name
-        self.rank=0
-        self.rank_chaneg=0
+        self.rank=rank
+        self.rank_change=0
         self.prv_point=0
-        self.diffpoint=0
-        self.conf=0
+        self.point=point
+        self.conf=conf
+        
+        self.conf=None
+
 
 teams=[]
+spcial_teams={"IR Iran":"Iran","Turkey":"Turkiye","Korea Republic":"South Korea","Côte d'Ivoire":"Ivory Coast","Congo DR":"DR Congo","Cabo Verde":"Cape Verde","Curacao":"Curaçao","USA":"United States"}
+
+df=pd.read_csv("./team_data/fifa_ranking-2024-06-20.csv")
+df=df[df["rank_date"]=="2024-06-20"]
 
 for tea in world_cup_2026_teams:
-    teams.append(team(tea))
+    row=df[df["country_full"] == tea]
+    if row.empty:
+        print("Missing team:", tea)
+        continue
+    if tea in spcial_teams:
+        tea=spcial_teams[tea]
+    row=row.iloc[0]
 
+    rank=int(row["rank"])
+    point=float(row["total_points"])
+    conf=row["confederation"]
 
+    teams.append(team(tea, rank, point, conf))
+   
 
+names=[]
 with sync_playwright() as p:
     
     browser=p.chromium.launch(headless=False,args=["--disable-blink-features=AutomationControlled"])
@@ -83,14 +106,65 @@ with sync_playwright() as p:
             soup=BeautifulSoup(html,"html.parser")
             
 
-            search_team=soup.select("td.hauptlink a")
+            search_team=soup.select("tr.odd, tr.even")
             
-        
-            for a in search_team:
-                name=a.get_text(strip=True)
-                if name:
-                    teams_name.append(name)
-            print(teams_name)
+            
+            
+            
+            j=0
+            world_cup_teams=teams.copy()
+            while(j<len(search_team) and len(world_cup_teams)>0):
+                name_tag=search_team[j].select_one("td.hauptlink a")
+                
+                rank_tag=search_team[j].select_one("td.zentriert.cp")
+                
+                point_tag=search_team[j].select_one("td.zentriert.hauptlink")
+                
+                print(name_tag)
+                
+                
+                if not name_tag or not point_tag or not rank_tag:
+                    print("somthing not found")
+                    
+                    j+=1
+                    continue
+
+                img=name_tag.select_one("img")
+                name=img["alt"]
+                
+                
+                
+                
+
+                rank=int(rank_tag.text.strip())
+                point=float(point_tag.text.strip())
+                print(name,rank,point)
+                print(name)
+                for te  in world_cup_teams:
+                    
+                    
+                    
+                    if te.name==name:
+                        te.rank_change=te.rank-rank
+                        te.rank=rank
+                        te.prv_point=te.point
+                        te.point=point
+                        with open("./team_data/fifa_ranking-2024-06-20.csv","a", newline="") as f:
+                            writer=csv.writer(f)
+                            writer.writerow([te.rank,te.name,te.name,te.point,te.prv_point,te.rank_change,te.conf,date])
+                        
+                        world_cup_teams.remove(te)
+                        print("a teams was add")
+                        break
+                    
+
+                j+=1    
+
+                
+
+                
+                
+            
 
 
                    
